ce46925ea27def80354a5b73cf4d2490cdbf18ac
emran <shuvoorba28@gmail.com>
2023-10-03 21:07:23 +0600

    private components created

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { Navigate } from "react-router-dom";
+import PropTypes from 'prop-types'
 
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext)
+    if(loading){
+        return <span className="loading loading-spinner loading-lg"></span>; 
+    }
 
     if(user){
         return children;
@@ -15,3 +19,7 @@ const PrivateRoute = ({ children }) => {
 };
 
 export default PrivateRoute;
+
+PrivateRoute.proptypes = {
+    children: PropTypes.node
+}
